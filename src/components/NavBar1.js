91c0dbd82c9bc11587e2a9f9885e1879a825750d
Catherine <catherineloy99@gmail.com>
2020-05-21 12:31:44 +0800

    Update navbar

--- src/components/NavBar1.js
+++ src/components/NavBar1.js
@@ -26,7 +26,7 @@ class NavBar extends React.Component {
     return (
       <div>
         <header>
-          <Router>
+          {/* <Router> */}
             <MDBNavbar fixed="top" dark expand="md" scrolling transparent>
               <Container>
                 <a href="#intro">
@@ -39,7 +39,7 @@ class NavBar extends React.Component {
                 {!this.state.isWideEnough && <MDBNavbarToggler onClick={this.onClick} />}
                 <MDBCollapse isOpen={this.state.collapse} navbar>
                   <MDBNavbarNav right>
-                        <Nav.Link id="move-down" as={Link} to="/reality-cast-landing-page/tutorial" >Tutorial</Nav.Link>
+                        <Nav.Link id="move-down">Tutorial</Nav.Link>
                         <Nav.Link  id="move-down" href="#pricing">Pricing</Nav.Link>
                         <Nav.Link  id="move-down" href="https://www.youtube.com/">Log In</Nav.Link>
                         
@@ -51,10 +51,10 @@ class NavBar extends React.Component {
                 </MDBCollapse>
               </Container>
             </MDBNavbar>
-            <Switch>
+            {/* <Switch>
               <Route exact path='/reality-cast-landing-page/tutorial' component={Tutorial} />
             </Switch>
-          </Router>
+          </Router> */}
         </header>
       </div>
     );
