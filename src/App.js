543d2003923bc89ffec6cf6e5763377d7329ac36
Catherine <catherineloy99@gmail.com>
2020-05-21 13:08:31 +0800

    redo navbar

--- src/App.js
+++ src/App.js
@@ -9,23 +9,46 @@ import './style.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
+// import NavBar from './components/NavBar.js'
 import NavBar from './components/NavBar1.js'
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from './components/Home.js'
-import Tutorial from './components/Tutorial.js'
+import Features from './components/Features.js';
+import FeaturedPresentations from './components/FeaturedPresentations';
+import Hero from './components/Hero';
+// import Pricing from './components/Pricing';
+import Pricing1 from './components/Pricing1';
+import HowItWorks from './components/HowItWorks';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <NavBar/>
+      <header>
+
+        <Hero/>
+
+      </header>
+
+      <main className="bg mt-5">
         <div>
-          <Switch>
-          <NavBar/>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/tutorial" component={Tutorial} />
-          </Switch>
+
+            <FeaturedPresentations/>
+        
+            <hr className="container my-5 yellow"></hr>
+
+            <Features/>
+
+            <hr className="container my-5 invisible"></hr>
+
+            <HowItWorks/>
+
+            <Pricing1/>
+
         </div>
-      </Router>
+    </main>
+
+    <Footer/>
+    
     </div>
   );
 }
--- src/components/NavBar1.js
+++ src/components/NavBar1.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBCollapse } from 'mdbreact';
 import { Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import {Nav} from 'react-bootstrap';
 import './navbar.css';
-// import Tutorial from './Tutorial.js';
 
 class NavBar extends React.Component {
   constructor(props) {
@@ -26,28 +25,21 @@ class NavBar extends React.Component {
     return (
       <div>
         <header>
-          {/* <Router> */}
+          <Router>
             <MDBNavbar fixed="top" dark expand="md" scrolling transparent>
               <Container>
+                <a href="#intro">
                 <MDBNavbarBrand>
                   <strong>
-                    <img src="https://raw.githubusercontent.com/Hiverlab/reality-cast-landing-page/master/public/images/logo.png?token=AMBYZPV4VS26XBERFMA5X4C6YS474" height="50px" width="130px" alt=""/>
+                    <img src="/images/logo.png" height="50px" width="130px" alt=""/>
                   </strong>
                 </MDBNavbarBrand>
+                </a>
                 {!this.state.isWideEnough && <MDBNavbarToggler onClick={this.onClick} />}
                 <MDBCollapse isOpen={this.state.collapse} navbar>
                   <MDBNavbarNav right>
-                    <Link to="/">
-                      <Nav.Link id="move-down">Home</Nav.Link>
-                    </Link>
-                    <Link to="/tutorial">
-                      <Nav.Link id="move-down">Tutorial</Nav.Link>
-                    </Link>
-                        
-                        
                         <Nav.Link  id="move-down" href="#pricing">Pricing</Nav.Link>
                         <Nav.Link  id="move-down" href="https://www.youtube.com/">Log In</Nav.Link>
-                        
 
                         <button id="navbar-button" type= "button">Sign-Up</button>
                         
@@ -56,10 +48,7 @@ class NavBar extends React.Component {
                 </MDBCollapse>
               </Container>
             </MDBNavbar>
-            {/* <Switch>
-              <Route exact path='/reality-cast-landing-page/tutorial' component={Tutorial} />
-            </Switch>
-          </Router> */}
+          </Router>
         </header>
       </div>
     );
