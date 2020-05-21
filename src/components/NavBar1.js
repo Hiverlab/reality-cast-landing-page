913ef9af834f85601cbc77f6a32e56005d92eecc
Catherine <catherineloy99@gmail.com>
2020-05-21 12:48:10 +0800

    navbar

--- src/components/NavBar1.js
+++ src/components/NavBar1.js
@@ -37,7 +37,13 @@ class NavBar extends React.Component {
                 {!this.state.isWideEnough && <MDBNavbarToggler onClick={this.onClick} />}
                 <MDBCollapse isOpen={this.state.collapse} navbar>
                   <MDBNavbarNav right>
-                        <Nav.Link id="move-down" href="https://https://hiverlab.github.io/reality-cast-landing-page/tutorial">Tutorial</Nav.Link>
+                    <Link to="/reality-cast-landing-page">
+                      <Nav.Link id="move-down">Home</Nav.Link>
+                    </Link>
+                    <Link to="/reality-cast-landing-page/tutorial">
+                      <Nav.Link id="move-down">Tutorial</Nav.Link>
+                    </Link>
+                        
                         
                         <Nav.Link  id="move-down" href="#pricing">Pricing</Nav.Link>
                         <Nav.Link  id="move-down" href="https://www.youtube.com/">Log In</Nav.Link>
